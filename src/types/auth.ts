ee113c63fb87217cf98781ad65cec1c21f53a17f
ELIAS <elias@MacBook-Air-de-ELIAS.local>
2026-09-23 13:23:10 +0200

    nouvelle branche

---
+++ src/types/auth.ts
@@ -0,0 +1,11 @@
+import type { User } from './user'
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse extends User {
+  token: string;
+  refreshToken: string;
+}
